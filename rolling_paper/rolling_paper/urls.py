ccbea3feb7512f345f22462591bd52df7f85c993
hayeon-kim <khayeon0824@gmail.com>
2022-05-27 01:17:43 +0900

    add rolling app

--- rolling_paper/rolling_paper/urls.py
+++ rolling_paper/rolling_paper/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rolling.views import RollingAPI
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/rolling/', RollingAPI.as_view())
 ]
